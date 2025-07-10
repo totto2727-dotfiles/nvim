1d9ee5cb247751a57de17726e5eda08acde37865
totto2727 <kaihatu.totto2727@gmail.com>
2025-07-10 21:46:37 +0900

    feat: add render-markdown.nvim config

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        border = "thick",
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+  },
+}
