391fbf8111a9fd1f47d5ec6e13613d78b2b769a3
totto2727 <kaihatu.totto2727@gmail.com>
2026-07-22 02:02:44 +0900

    feat(markdown): preserve code fence delimiters

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        conceal_delimiters = false,
+        language = false,
+        border = "none",
+      },
+    },
+  },
+}
